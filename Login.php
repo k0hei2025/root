728a4c3d7a13ad9b41024ef2368b8af2606c067e
k0hei2025 <vaibhavvermaonline@gmail.com>
2020-03-18 22:34:21 +0530

    dynamic

---
+++ Login.php
@@ -0,0 +1,24 @@
+<?php
+$con = mysqli_connect('127.0.0.1','root','');
+if (!$con)
+{
+  echo 'Not connected To Server';
+}
+if (!mysqli_select_db($con,'food'))
+{
+  echo 'Database Not Selected';
+}
+$Email = $_POST['Email'];
+$pass = $_POST['Password'];
+$sql = "INSERT INTO login VALUES ('$Email','$pass')";
+if (!mysqli_query($con,$sql))
+{
+  echo mysqli_error($con);
+} 
+else
+{
+  echo 'Inserted';
+}
+header("refresh:2; url=login.html");
+?>
+
